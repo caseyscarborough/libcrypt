ba38c54ac548eecd665f3bd1bbe93d7a7894c3ec
Casey Scarborough <caseyscarborough@gmail.com>
2014-01-17 14:13:58 -0500

    Add Base64 encode/decode methods.

--- crypt.c
+++ crypt.c
@@ -1,6 +1,7 @@
 #include "crypt.h"
 
-char *rot13(char *s) {
+char *rot13(char *s)
+{
     for(int i = 0; s[i] != '\0'; i++) {
         if (isupper(s[i]))
             s[i] += s[i] < 'N' ? 13 : -13;
@@ -10,7 +11,8 @@ char *rot13(char *s) {
     return s;
 }
 
-char *rot47(char *s) {
+char *rot47(char *s)
+{
     for (int i = 0; s[i] != '\0'; i++) {
         if (s[i] > 32 && s[i] < 80)
             s[i] += 47;
@@ -18,4 +20,71 @@ char *rot47(char *s) {
             s[i] -= 47;
     }
     return s;
+}
+
+void build_decoding_table()
+{
+    decoding_table = malloc(256);
+    for (int i = 0; i < 64; i++)
+        decoding_table[(unsigned char) encoding_table[i]] = i;
+}
+
+char *base64_encode(const unsigned char *data, size_t input_length, size_t *output_length)
+{
+    *output_length = 4 * ((input_length + 2) / 3);
+    char *encoded_data = malloc(*output_length);
+    if (encoded_data == NULL) return NULL;
+
+    for (int i = 0, j = 0; i < input_length;) {
+        uint32_t octet_a = i < input_length ? (unsigned char)data[i++] : 0;
+        uint32_t octet_b = i < input_length ? (unsigned char)data[i++] : 0;
+        uint32_t octet_c = i < input_length ? (unsigned char)data[i++] : 0;
+
+        uint32_t triple = (octet_a << 0x10) + (octet_b << 0x08) + octet_c;
+
+        encoded_data[j++] = encoding_table[(triple >> 3 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 2 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 1 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 0 * 6) & 0x3F];
+    }
+
+    for (int i = 0; i < mod_table[input_length % 3]; i++)
+        encoded_data[*output_length - 1 - i] = '=';
+    return encoded_data;
+}
+
+
+unsigned char *base64_decode(const char *data, size_t input_length, size_t *output_length)
+{
+    if (decoding_table == NULL) build_decoding_table();
+    if (input_length % 4 != 0) return NULL;
+
+    *output_length = input_length / 4 * 3;
+    if (data[input_length - 1] == '=') (*output_length)--;
+    if (data[input_length - 2] == '=') (*output_length)--;
+
+    unsigned char *decoded_data = malloc(*output_length);
+    if (decoded_data == NULL) return NULL;
+
+    for (int i = 0, j = 0; i < input_length;) {
+        uint32_t sextet_a = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+        uint32_t sextet_b = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+        uint32_t sextet_c = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+        uint32_t sextet_d = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+
+        uint32_t triple = (sextet_a << 3 * 6)
+        + (sextet_b << 2 * 6)
+        + (sextet_c << 1 * 6)
+        + (sextet_d << 0 * 6);
+
+        if (j < *output_length) decoded_data[j++] = (triple >> 2 * 8) & 0xFF;
+        if (j < *output_length) decoded_data[j++] = (triple >> 1 * 8) & 0xFF;
+        if (j < *output_length) decoded_data[j++] = (triple >> 0 * 8) & 0xFF;
+    }
+    return decoded_data;
+}
+
+void base64_cleanup()
+{
+    free(decoding_table);
 }
--- test.c
+++ test.c
@@ -26,6 +26,14 @@ int main(int argc, char *argv[])
         printf("ROT47: %s\n", rot47(a));
         
         free(a);
+
+        char *name = "Casey Adam Scarborough";
+        size_t input_len = strlen((const char *)name);
+        size_t output_len = (size_t)(input_len * 4 / 3);
+        char * base64_e_name = base64_encode((const unsigned char *) name, input_len, &output_len);
+        unsigned char * base64_d_name = base64_decode(base64_e_name, output_len, &input_len);
+        printf("Encode: %s\nDecode: %s\n", base64_e_name, base64_d_name);
+        base64_cleanup();
     }
     return 0;
 }
